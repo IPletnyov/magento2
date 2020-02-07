3ee2a6f8af7e2cbf8e8c68bbe21de61a6f79ff1d
ivan.pletnyov <ivan.pletnyov@transoftgroup.com>
2020-02-07 17:32:36 +0200

    Process NewsFrontendUi module

---
+++ app/code/Ivan/NewsFrontendUi/Controller/Index/Index.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ivan\NewsFrontendUi\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\Result\JsonFactory;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    /**
+     * @var PageFactory
+     */
+    private $pageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory
+    ) {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function execute(): ResultInterface
+    {
+        return $this->pageFactory->create();
+    }
+}
